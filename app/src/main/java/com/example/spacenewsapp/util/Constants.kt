198710fbd95ad22fd07243df744330a83fc81216
rafo <rafogasparyan@gmail.com>
2021-02-18 14:43:39 +0400

    changed project url

--- app/src/main/java/com/example/spacenewsapp/util/Constants.kt
+++ app/src/main/java/com/example/spacenewsapp/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.spacenewsapp.util
 class Constants {
     companion object {
         const val PROFILE_URL = "https://scontent.fevn5-1.fna.fbcdn.net/v/t1.0-9/46454474_2391999477538906_1994071758398816256_o.jpg?_nc_cat=103&ccb=3&_nc_sid=09cbfe&_nc_ohc=XHR6FpOUBuoAX_g2FcW&_nc_ht=scontent.fevn5-1.fna&oh=0d3870c2f2992f74d7ef835bf3a474f2&oe=605293A7"
-        const val PROJECT_URL = "https://github.com/rafo94/MyPet"
+        const val PROJECT_URL = "https://github.com/rafo94/SpaceNewsApp"
         const val FACEBOOK_URL = "https://www.facebook.com/gasparyan94"
         const val LINKEDIN_URL = "rafo-gasparyan-16468ba1"
         const val GITHUB_URL = "https://github.com/rafo94/"
